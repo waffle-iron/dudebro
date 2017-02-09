1281d194fc1a1c659e4fe98bd765af7d11b53775
Mel Pearlmutter <jpearlmutter95@gmail.com>
2017-02-09 08:12:59 -0700

    JSHint Doesn't Like These Extra Semicolons
    Neither do I.

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function changeNick(nickname) {
         userID: "278675895919837185"
     });
     console.log('Nickname automatically changed to ' + nickname + '');
-};
+}
 
 function reply(channelID, userID, message) {
     client.sendMessage({
@@ -38,7 +38,7 @@ function reply(channelID, userID, message) {
         message: '<@!' + userID + '> ' + message
     });
     console.log('----------------\n\n' + message);
-};
+}
 
 function dudeTalkBack(channelID, userID, message) {
     client.sendMessage({
@@ -46,19 +46,19 @@ function dudeTalkBack(channelID, userID, message) {
         message: '<@!' + userID + '> ' + message
     });
     console.log(message);
-};
+}
 
 function busy(isBusy) {
     if (isBusy === true) {
         idleTime = Date.now();
     } else {
         idleTime = null;
-    };
+    }
 
     client.setPresence({
         idle_since: idleTime
     });
-};
+}
 
 function reset() {
     fuelClientID = null;
@@ -78,7 +78,7 @@ function dudeLogin(userID) {
     for (var i = 0; i < standbyList.length; i++) {
         console.log(standbyList[i]);
         if (standbyList[i] === userID) return "You\'re already on duty!";
-    };
+    }
     standbyList.push(userID);
     return ("You are now set as **on-duty**.");
 }
@@ -88,10 +88,10 @@ function dudeLogout(userID) {
         if (standbyList[i] === userID) {
             standbyList.splice(i, i + 1);
             return ("You are now set as **off-duty**.");
-        };
-    };
+        }
+    }
     return ("You\'re already off duty!");
-};
+}
 
 // ================ BOT HANDLING ================ //
 
@@ -107,7 +107,7 @@ client.on('disconnect', function(errMsg, code) {
         console.log('Failed to connect to Discord.\nCheck your Internet connection and bot token, and then try again.\n' + code + ': ' + errMsg);
     } else {
         console.log('Failed to connect to Discord.\n' + code + ': ' + errMsg);
-    };
+    }
 });
 
 
@@ -126,7 +126,7 @@ client.on('message', function(user, userID, channelID, message, event) {
             statement = '**Hey there, ' + user + '!** Let\'s get you out of trouble.\n\nFor starters, would you please tell me your commander name?\n*You\'ll need to include "CMDR" in your name, otherwise I won\'t detect your name!*\n\nIf at any point in time you would like to cancel dispatch, send `cancel`.';
             reply(channelID, userID, statement);
             busy(true);
-        };
+        }
 
         if (userID === fuelClientID && fuelClientState === 1) {
             // name updater and system prompt
@@ -135,11 +135,11 @@ client.on('message', function(user, userID, channelID, message, event) {
                     statement = '**' + message + '** it is! If that doesn\'t look right, please send your name again.\n\nNext, I need to know which system you\'re in. Make sure to include "system" at the end so I can detect it.\nExample: `Eravate system`';
                 } else {
                     statement = 'Name updated to **' + message + '**.';
-                };
+                }
                 fuelClientName = message;
                 reply(channelID, userID, statement);
                 busy(true);
-            };
+            }
 
             // system updater and platform prompt
             if (message.endsWith(" system") && userID === fuelClientID) {
@@ -149,11 +149,11 @@ client.on('message', function(user, userID, channelID, message, event) {
                     statement = 'I have noted your system as **' + message + '**! If that doesn\'t look right, please send it again.\n\nNext, I need to know which platform you\'re playing on. Respond with either `pc`, `ps4` or `xbox` to let me know!';
                 } else {
                     statement = 'System updated to **' + message + '**.';
-                };
+                }
                 fuelClientSystem = message;
                 reply(channelID, userID, statement);
                 busy(true);
-            };
+            }
 
             // platform updater and O2 prompt
             if (message === 'xbox' || message === 'ps4' || message === 'pc') {
@@ -162,11 +162,11 @@ client.on('message', function(user, userID, channelID, message, event) {
                     statement = 'Alright, we\'ll make sure to send a fuel dude on the **' + message + '** platform.\n\nLastly, are you running on life support (blue countdown in the upper right)? Please respond with `yes` or `no`.';
                 } else {
                     statement = 'I have noted your platform change to **' + message + '**.';
-                };
+                }
                 fuelClientRealm = message;
                 reply(channelID, userID, statement);
                 busy(true);
-            };
+            }
 
             // O2 updater
             if (message === 'yes' || message === 'no') {
@@ -179,15 +179,15 @@ client.on('message', function(user, userID, channelID, message, event) {
                 reply(channelID, userID, statement);
                 dispatchTransition();
                 busy(true);
-            };
+            }
 
             if (message === 'cancel') {
                 busy(false);
                 reset();
                 statement = 'Okay, I\'ve cancelled fuel dispatch. Have a great day!';
                 reply(channelID, userID, statement);
-            };
-        };
+            }
+        }
 
         // ---------------- DISPATCH PROGRAM ---------------- //
 
@@ -197,7 +197,7 @@ client.on('message', function(user, userID, channelID, message, event) {
                 // jump communication
                 if (message.endsWith('j')) {
                     reply('roger.');
-                };
+                }
                 // =+ lexicon += //
                 // beacon (bc+/bc-)
 
@@ -229,26 +229,26 @@ client.on('message', function(user, userID, channelID, message, event) {
                 // 
 
 
-            };
-        };
+            }
+        }
 
         if (true) { // placeholder for fueldude role check
             if (message === "standby") {
                 reply(channelID, userID, dudeLogin(userID));
-            };
+            }
 
             if (message === ("offline")) {
                 reply(channelID, userID, dudeLogout(userID));
-            };
-        };
-    };
+            }
+        }
+    }
 
 
     // debugging commands, which will work on any channel
 
     if (message === "ping") {
         reply(channelID, userID, 'pong!');
-    };
+    }
 
     if (message.endsWith(" slice test")) {
         endSlice = message.lastIndexOf(" slice test");
@@ -259,13 +259,13 @@ client.on('message', function(user, userID, channelID, message, event) {
     if (message === "sudo shutdown -h now") {
         changeNick("Offline");
         client.disconnect();
-    };
+    }
 
     if (message === 'dispatch status test') {
         busy(true);
-    };
+    }
 
     if (message === 'dispatch ended test') {
         busy(false);
-    };
+    }
 });
