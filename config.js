ae64c114ee0d480910c856c4a9b7f1bef7501198
Mel Pearlmutter <jpearlmutter95@gmail.com>
2017-02-10 20:37:07 -0700

    More Config Options

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 module.exports = {
     bot: {
         token: 'paste token between apostrophes',
-        owner: 'your user ID'
+        owner: 'your user ID',
+        lexchan: 'fueldudes channel ID',
+        ripList: ['pepperoni', 'primitive putanesca', 'potent peas', 'pumpkin pie', 'projectile puke', 'paprika', 'piss', 'peppermint phalanxes', 'peanuts', 'pistachios']
     }
 };
